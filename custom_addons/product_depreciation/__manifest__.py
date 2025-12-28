9aad0439566c9e441ad2cb3046d271b345d6bdd4
leonmwila <leonmwila47@gmail.com>
2025-12-28 08:08:58 +0200

    Added product_depreciation module

---
+++ custom_addons/product_depreciation/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Product Depreciation',
+    'version': '1.0',
+    'category': 'Inventory/Accounting',
+    'summary': 'Adds depreciation functionality to Products and Lot/Serial Numbers',
+    'description': """
+        This module adds depreciation fields and calculations to Products and Lot/Serial Numbers.
+        
+        Features:
+        - Depreciation method and useful life on Products
+        - Fair value, disposal date, disposal price on Lot/Serial Numbers
+        - Automatic calculation of depreciation amount and Net Book Value (NBV)
+        - Auto-population of disposal date when disposal price is entered
+    """,
+    'depends': ['product', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_template_views.xml',
+        'views/stock_lot_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
+
